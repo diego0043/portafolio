36573af00fb659287a96b942792baac7401cdc43
Diego Valencia <ve18007@ues.edu.sv>
2023-04-28 09:12:10 -0600

    reaciendo parte de proyectos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,128 +4,63 @@ import heroes from "../assets/images/heroesApp.png";
 export const Projects = () => {
   return (
     <>
-      <div className="container-fluid container-projects">
+      <div
+        id="carouselExampleDark"
+        className="carousel carousel-dark slide container-carousel container-fluid shadow p-3 mb-5 bg-body rounded"
+        data-bs-ride="carousel"
+      >
         <div className="row">
-          <span className="title-projects">My Portfolio</span>
+          <span className="text-center">Projects</span>
         </div>
-        <div className="container-cols">
-          <div className="row">
-            <div className="col-sm-12 col-md-3 col-lg-3 col-xl-3">
-              <div className="card-project">
-                <div className="row">
-                  <div className="image-card">
-                    <img className="image" src={seus} alt="" />
-                  </div>
-                </div>
-                <div className="row">
-                  <span className="title-card">web voting application</span>
-                </div>
-                <div className="row">
-                  <p className="paragraph">
-                    This application was created specifically for the students
-                    of the Universidad de El Salvador with the aim of
-                    modernizing the voting system and introducing new
-                    technologies to enhance the voting process.
-                  </p>
-                </div>
-                <div className="row container-btn">
-                  <a className="btn-github">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="22"
-                      height="22"
-                      fill="currentColor"
-                      class="icon-btn-project"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-                    </svg>
-                    <span className="text-btn">Github</span>
-                  </a>
-                  <a className="btn-demo">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="22"
-                      height="22"
-                      fill="currentColor"
-                      class="icon-btn-project"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M0 4s0-2 2-2h12s2 0 2 2v6s0 2-2 2h-4c0 .667.083 1.167.25 1.5H11a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1h.75c.167-.333.25-.833.25-1.5H2s-2 0-2-2V4zm1.398-.855a.758.758 0 0 0-.254.302A1.46 1.46 0 0 0 1 4.01V10c0 .325.078.502.145.602.07.105.17.188.302.254a1.464 1.464 0 0 0 .538.143L2.01 11H14c.325 0 .502-.078.602-.145a.758.758 0 0 0 .254-.302 1.464 1.464 0 0 0 .143-.538L15 9.99V4c0-.325-.078-.502-.145-.602a.757.757 0 0 0-.302-.254A1.46 1.46 0 0 0 13.99 3H2c-.325 0-.502.078-.602.145z" />
-                    </svg>
-                    <span className="text-btn">Demo</span>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-12 col-md-3 col-lg-3 col-xl-3">
-              <div className="card-project">
-                <div className="row">
-                  <div className="image-card">
-                    <img className="image" src={heroes} alt="" />
-                  </div>
-                </div>
-                <div className="row">
-                  <span className="title-card">Superhero Finder</span>
-                </div>
-                <div className="row">
-                  <p className="paragraph">
-                    Looking for your favorite DC and Marvel superheroes? Our app
-                    has a user-friendly interface and a comprehensive database
-                    via API to easily search and discover heroes like Spider-Man
-                    and Wonder Woman.
-                  </p>
-                </div>
-                <div className="row container-btn">
-                  <a className="btn-github">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="22"
-                      height="22"
-                      fill="currentColor"
-                      class="icon-btn-project"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-                    </svg>
-                    <span className="text-btn">Github</span>
-                  </a>
-                  <a className="btn-demo">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="22"
-                      height="22"
-                      fill="currentColor"
-                      class="icon-btn-project"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M0 4s0-2 2-2h12s2 0 2 2v6s0 2-2 2h-4c0 .667.083 1.167.25 1.5H11a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1h.75c.167-.333.25-.833.25-1.5H2s-2 0-2-2V4zm1.398-.855a.758.758 0 0 0-.254.302A1.46 1.46 0 0 0 1 4.01V10c0 .325.078.502.145.602.07.105.17.188.302.254a1.464 1.464 0 0 0 .538.143L2.01 11H14c.325 0 .502-.078.602-.145a.758.758 0 0 0 .254-.302 1.464 1.464 0 0 0 .143-.538L15 9.99V4c0-.325-.078-.502-.145-.602a.757.757 0 0 0-.302-.254A1.46 1.46 0 0 0 13.99 3H2c-.325 0-.502.078-.602.145z" />
-                    </svg>
-                    <span className="text-btn">Demo</span>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-12 col-md-3 col-lg-3 col-xl-3">
-              <div className="card-project">
-                <div className="row">
-                  <div className="image-card">
-                    <img src="" alt="" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-12 col-md-3 col-lg-3 col-xl-3">
-              <div className="card-project">
-                <div className="row">
-                  <div className="image-card">
-                    <img src="" alt="" />
-                  </div>
-                </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-bs-interval="10000">
+            <img src={seus} className="image-carousel" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <div classNameName="row">
+                <button className="btn-github">Github</button>
+                <button className="btn-demo">Web</button>
               </div>
             </div>
           </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={seus} className="image-carousel" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <button className="btn-github">Github</button>
+              <button className="btn-demo">Web</button>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={seus} className="image-carousel" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <button className="btn-github">Github</button>
+              <button className="btn-demo">Web</button>
+            </div>
+          </div>
         </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleDark"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleDark"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
     </>
   );
