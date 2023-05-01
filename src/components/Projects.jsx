474e184246db99dc74bfa1aece61d84412b78990
Diego Valencia <ve18007@ues.edu.sv>
2023-05-01 11:04:27 -0600

    projects terminado

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,15 @@
 import { data } from "../helpers/data.js";
 import { CardProjects } from "./CardProjects.jsx";
 export const Projects = () => {
-  
   return (
     <>
-      <div className="container-projects">
-        {data.forEach((project) => {
-          <div key={project.id} className="container-card">
-            {project.title}
-          </div>
+      
+      <div className="container-projects container row">
+      <div className="row container-fluid">
+        <span data-aos="fade-up" className="title-services">Projects</span>
+      </div>
+        {data.map((project) => {
+          return <CardProjects key={project.id} {...project} />;
         })}
       </div>
     </>
