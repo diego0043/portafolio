77220e57aba547ab7289dab611fe7e1bccc24239
Diego Valencia <ve18007@ues.edu.sv>
2023-05-12 20:32:38 -0600

    encapsulacion de css y agregando efecto water al title home

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,29 +1,36 @@
 import React from "react";
 import img from "../assets/Frame.png";
-import cv from "../cv/currículo-diego.valencia.pdf"
+import cv from "../cv/currículo-diego.valencia.pdf";
 
 export const HomeContainer = () => {
   return (
     <>
-      <div className="row container-fluid home" data-aos="fade-up" data-aos-duration="500">
+      <div
+        className="row container-fluid home"
+      >
         <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 animate__animated animate__fadeInLeft container-fluid container-text-home">
-          <div className="row mt-2">
-            <h1 className="text-title-home">DIEGO VALENCIA</h1>
+          <div className="row mt-2 content">
+            <h2 className="text-title-home">DIEGO</h2>
+            <h2 className="text-title-home">DIEGO</h2>
           </div>
-          <div className="row mt-3 text-p-home">
+          <div className="row content content-2">
+            <h2 className="text-title-home">VALENCIA</h2>
+            <h2 className="text-title-home">VALENCIA</h2>
+          </div>
+          <div className="row text-p-home">
             <p>
               Hello👋, I´ am a systems engineer and web developer, I have
-              experience in front-end development, I am passionate about
+              experience in front-end development and QA, I am passionate about
               technology and programming, I am always learning new things and I
               am always looking for new challenges.
             </p>
           </div>
           <div className="row">
             <div className="col-4">
-            <a href={cv} download>
-              <button className="btn-home-description shadow">
-                Download CV
-              </button>
+              <a href={cv} download>
+                <button className="btn-home-description shadow">
+                  Download CV
+                </button>
               </a>
             </div>
           </div>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 AOS.init();
 import "./main.css";
+import "../src/styles/home.css";
+import "../src/styles/service.css";
+import "../src/styles/projects.css";
+import "../src/styles/navbar.css";
 import 'animate.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
